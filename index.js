cb07e90a0b69e3eefc2722ff6c51b87ffad6ff1e
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-05 13:45:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstDigit = +prompt("Enter the fist digit");
+
+let secondDigit = +prompt("Enter the second digit");
+
+alert(firstDigit + secondDigit);
+alert(firstDigit - secondDigit);
+alert(firstDigit * secondDigit);
+alert(firstDigit / secondDigit);
+
